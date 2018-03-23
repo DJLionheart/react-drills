ee19e729f89405351d3c0b12242462e795665e80
Paul Peterson <paulpeterson@Pauls-MacBook-Pro.local>
2018-03-23 17:51:11 -0600

    app2-practice

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+    super();
+
+    this.state = {
+      fableheim_heroes: ['Balthazar', 'Erdessanor', 'Galerian', 'Nymphatrina', 'Grizwelt', 'Samfrath']
+    }
+  }
   render() {
+
+    const {fableheim_heroes} = this.state
+
+    const heroList = fableheim_heroes.map( (e, i, arr) => (
+      <div key={e}>
+        <h2>{e}</h2>
+
+      </div>
+    ))
+    // const heroList = this.state.fableheim_heroes.map( (e, i, arr) => (
+    //   <div key={e}>
+    //     <h2>{e}</h2>
+    //   </div>
+    // ))
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Ye Olde Heroes of Fableheim:</h1>
+        {heroList}
       </div>
     );
   }
